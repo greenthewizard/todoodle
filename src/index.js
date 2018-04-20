f59a4e842f11b08732ae548ba3ce92fb1c0e0a27
buddytherat <buddytherat@gmail.com>
2018-04-19 23:32:59 -0500

    Created template for rending task data. Seeded with mock data.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import './js/styleBarrel.js';
 import * as taskFactory from './js/taskFactory.js';
 import render from './js/render.js';
 
-let task1 = taskFactory.createTask('task1', 'hey');
-let task2 = taskFactory.createTask('task2', 'hey');
-let task3 = taskFactory.createTask('task3', 'hey');
-let task4 = taskFactory.createTask('task4', 'hey');
-let task5 = taskFactory.createTask('task5', 'hey');
-let task6 = taskFactory.createTask('task6', 'hey');
-let task7 = taskFactory.createTask('task7', 'hey');
-let task8 = taskFactory.createTask('task8', 'hey');
+let task1 = taskFactory.createTask('Finish todo app');
+    let task4 = taskFactory.createTask('Make the rest of the app');
+    let task5 = taskFactory.createTask('Learn how to make the rest of the app');
+        let task7 = taskFactory.createTask('Learn javascript');
+        let task8 = taskFactory.createTask('Learn HTML/CSS');
+let task2 = taskFactory.createTask('Grocery List');
+    let task6 = taskFactory.createTask('Buy Milk');
+let task3 = taskFactory.createTask('Make more lists');
 
 task4.setParent(task1);
 task5.setParent(task1);
@@ -17,9 +17,15 @@ task6.setParent(task2);
 task7.setParent(task5);
 task8.setParent(task5);
 
-let sorted = taskFactory.getSortedTasks();
-sorted.forEach(task => {
-    let dash = '='
-    let dashes = dash.repeat(taskFactory.getGenerationOf(task));
-    console.log(dashes + task.getTitle());
-});
+let view = {
+    tasks: taskFactory.getSortedTasks(),
+    hasChildren: function() {
+        return taskFactory.getChildrenFor(this).length > 0;
+    },
+    getIndent: function() {
+        return taskFactory.getGenerationOf(this) * 2 + 'rem';
+    }
+}
+
+let $ul = document.querySelector('#todo-list');
+render($ul, 'templates/tasklist.mst', view);
