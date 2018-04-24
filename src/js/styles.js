cc88549fff4882b986935b8d5d29e80a6289abec
buddytherat <buddytherat@gmail.com>
2018-04-23 22:18:12 -0500

    Renamed styleBarrel.js to styles.js.

---
+++ src/js/styles.js
@@ -0,0 +1,19 @@
+import '.././styles/style.scss';
+import fontawesome from '@fortawesome/fontawesome';
+import faEdit from '@fortawesome/fontawesome-free-regular';
+import faClock from '@fortawesome/fontawesome-free-regular';
+import faLightbulb from '@fortawesome/fontawesome-free-regular';
+import faCheck from '@fortawesome/fontawesome-free-solid';
+import faAngleDown from '@fortawesome/fontawesome-free-solid';
+import faAngleRight from '@fortawesome/fontawesome-free-solid';
+import faPlusCircle from '@fortawesome/fontawesome-free-solid';
+
+fontawesome.library.add(
+    faEdit,
+    faCheck,
+    faAngleDown,
+    faAngleRight,
+    faClock,
+    faLightbulb,
+    faPlusCircle
+);
